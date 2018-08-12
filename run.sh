0686599744584da78e9f21b033a1dc8917d068d5
ilix <src@ilix.se>
2018-08-12 17:57:12 +0200

    task: 1.13

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /mc
-curl -o server.jar https://launcher.mojang.com/mc/game/1.12/server/8494e844e911ea0d63878f64da9dcc21f53a3463/server.jar
+curl -o server.jar https://launcher.mojang.com/mc/game/1.13/server/d0caafb8438ebd206f99930cfaecfa6c9a13dca0/server.jar
 
 rm -fr /mc/eula.txt
 java -Xmx2G -jar /mc/server.jar
