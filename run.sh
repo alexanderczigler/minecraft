90abe5a566cdafa6e773fa64d30d229e543b534d
ilix <src@ilix.se>
2018-08-12 17:20:08 +0200

    fix: preparations for tag-based images

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+cd /mc
+curl https://launcher.mojang.com/mc/game/1.13/server/d0caafb8438ebd206f99930cfaecfa6c9a13dca0/server.jar
+
 rm -fr /mc/eula.txt
-java -Xmx2G -jar /mc/minecraft_server.jar
+java -Xmx2G -jar /mc/server.jar
 sed -i 's/false/true/g' /mc/eula.txt
-java -Xmx2G -jar /mc/minecraft_server.jar
+java -Xmx2G -jar /mc/server.jar
