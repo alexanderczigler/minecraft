99a2e127b407ba57ad980cf251ac02b724d461b8
ilix <src@ilix.se>
2018-08-12 17:51:52 +0200

    fix: whoopsie

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /mc
-curl https://launcher.mojang.com/mc/game/1.13/server/d0caafb8438ebd206f99930cfaecfa6c9a13dca0/server.jar
+curl -o server.jar https://launcher.mojang.com/mc/game/1.13/server/d0caafb8438ebd206f99930cfaecfa6c9a13dca0/server.jar
 
 rm -fr /mc/eula.txt
 java -Xmx2G -jar /mc/server.jar
