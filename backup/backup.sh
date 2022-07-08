7c552d83f21ae86a043757924dbcc90650c9361b
Alexander Czigler <dev@ilix.se>
2022-07-08 16:11:40 +0200

    feat: backup gz

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,10 +2,19 @@
 echo "Running backup script..."
 
 function rcon {
+  echo "RCON: $1"
   /tmp/mcrcon -sc -H "$MINECRAFT_SERVER" -P "$MINERACFT_SERVER_PORT" -p "$MINECRAFT_RCON_PASSWORD" "$1"
 }
 
+BACKUP_NAME=$(date +\%Y-\%m-\%d_\%H\%M\%S).gz
+
+# Disable world writing
 rcon "save-off"
 rcon "save-all"
-rsync -av /minecraft rsync://$RSYNC_USER@$RSYNC_HOST:$RSYNC_PATH
+
+# Backup world
+tar zcvf "/backup/$BACKUP_NAME" /mc/world
+
+# Enable world writing
 rcon "save-on"
+echo "Done!"
